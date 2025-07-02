93dbe0dd944d99ed12b84931aef55e3e6aeb0ea0
Erick Matuck <51512047+erickcarvalhomatuck@users.noreply.github.com>
2025-07-02 17:20:02 -0300

    Delete api directory

--- api/index.py
+++
@@ -1,7 +0,0 @@
-from src.main_firebase import app
-
-# This is the entry point for Vercel
-# It imports the Flask app instance from src/main_firebase.py
-# Vercel will look for a 'app' variable here
-
-
